9f7a5daf0b793033959b53d85b2ce26341c7a3cd
Fahreddin TOKATLI <fahreddintokatli@ajancnc.com>
2022-06-08 10:51:19 +0300

    Add Line Class

---
+++ dist/line.js
@@ -0,0 +1,14 @@
+class Line {
+  constructor(startx, starty, endx,endy) {
+    this.startx = startx;
+    this.starty = starty;
+    this.endx = endx;
+    this.endy = endy;
+  }
+
+  get length(){
+    return Math.sqrt( (Math.abs(this.startx - this.endx) * Math.abs(this.startx - this.endx) ) + ( Math.abs(this.starty - this.endy) * Math.abs(this.starty - this.endy)));
+  }
+}
+
+export default Line;
